71aa91bd2f45cf0eda8910b572f38a3a721afb78
waxer59 <hnboyano@gmail.com>
2024-08-06 03:39:14 +0200

    remove astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
-import db from '@astrojs/db';
 import tailwind from '@astrojs/tailwind';
 import icon from "astro-icon";
 
@@ -8,5 +7,5 @@ import icon from "astro-icon";
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [db(), tailwind(), icon()]
+  integrations: [tailwind(), icon()]
 });
