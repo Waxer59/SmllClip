96f6433ddc365c566a415b5301a98fd38267907e
waxer59 <hnboyano@gmail.com>
2024-09-10 04:46:27 +0200

    add editors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
-import tailwind from '@astrojs/tailwind';
-import icon from "astro-icon";
+import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel/serverless'
+import tailwind from '@astrojs/tailwind'
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [tailwind(), icon()]
-});
+  integrations: [tailwind(), icon()],
+})
