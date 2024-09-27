26dd37fe817dcc111b7a2d3218d619f89d00c498
waxer59 <hnboyano@gmail.com>
2024-09-28 00:44:26 +0200

    improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
-import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify'
+
+import sitemap from '@astrojs/sitemap'
+
+const site = import.meta.env.FRONTEND_URL
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  site,
   adapter: netlify(),
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), sitemap()],
   devToolbar: {
     enabled: false
   }
